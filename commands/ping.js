cc0a1bcd690b2631581c897457164e19112d0d1e
renaism <zafitract@gmail.com>
2021-12-06 14:47:55 +0700

    Fix grammar in the description

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Check if bot is online.'),
+        .setDescription('Check if the bot is online.'),
     async execute(interaction) {
         await interaction.reply('Hello!');
     },
