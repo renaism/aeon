0164d8a006eb13e14b7d9b913ec86bb26e63105d
renaism <zafitract@gmail.com>
2021-12-08 12:53:29 +0700

    Add help command

---
+++ commands/help.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const config = require('../config.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription(`List all of ${config.name}'s commands.`),
+    async execute(interaction) {
+        const embed = new MessageEmbed()
+            .setAuthor('Aeon', interaction.client.user.avatarURL({ size: 16 }), config.homepage)
+            .setTitle('Commands');
+
+        let commandHelp = '';
+        interaction.client.commands.forEach(command => {
+            commandHelp += `**\`/${command.data.name}\`** ${command.data.description}\n`;
+        });
+        embed.setDescription(commandHelp);
+
+        await interaction.reply({ embeds: [embed] });
+    },
+};
